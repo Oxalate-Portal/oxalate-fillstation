5203f295cbacf0a8bfaf8861aa2737d7e5b2d0d1
Paul-Erik Törrönen <poltsi@poltsi.fi>
2026-05-30 18:36:35 +0300

    [frontend] Add rcComponentUtil.ts shim to fix the building of the app
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

---
+++ oxalate-fillstation-frontend/src/shims/rcComponentUtil.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-deprecated */
+
+export {default as useEvent} from '@rc-component/util/es/hooks/useEvent';
+export {default as useMergedState} from '@rc-component/util/es/hooks/useMergedState';
+export {default as useControlledState} from '@rc-component/util/es/hooks/useControlledState';
+export {
+    supportNodeRef,
+    supportRef,
+    useComposeRef,
+    composeRef,
+    fillRef,
+    getNodeRef,
+} from '@rc-component/util/es/ref';
+export {default as get} from '@rc-component/util/es/utils/get';
+export {default as set, merge, mergeWith} from '@rc-component/util/es/utils/set';
+export {default as warning, noteOnce} from '@rc-component/util/es/warning';
+export {default as omit} from '@rc-component/util/es/omit';
+export {default as toArray} from '@rc-component/util/es/Children/toArray';
+export {default as mergeProps} from '@rc-component/util/es/mergeProps';
+export {default as KeyCode} from '@rc-component/util/es/KeyCode';
+export {default as canUseDom} from '@rc-component/util/es/Dom/canUseDom';
+export {getDOM} from '@rc-component/util/es/Dom/findDOMNode';
+export {default as getScrollBarSize, getTargetScrollBarSize} from '@rc-component/util/es/getScrollBarSize';
+export {getFocusNodeList, triggerFocus} from '@rc-component/util/es/Dom/focus';
+export {default as isEqual} from '@rc-component/util/es/isEqual';
+export {default as isMobile} from '@rc-component/util/es/isMobile';
+export {default as isVisible} from '@rc-component/util/es/Dom/isVisible';
+export {default as pickAttrs} from '@rc-component/util/es/pickAttrs';
+export {default as raf} from '@rc-component/util/es/raf';
+export {default as useId, getId} from '@rc-component/util/es/hooks/useId';
+export {default as useLayoutEffect, useLayoutUpdateEffect} from '@rc-component/util/es/hooks/useLayoutEffect';
+export {default as useMemo} from '@rc-component/util/es/hooks/useMemo';
+export {default as useState} from '@rc-component/util/es/hooks/useState';
+export {injectCSS, removeCSS, updateCSS, clearContainerCache} from '@rc-component/util/es/Dom/dynamicCSS';
+export {inShadow, getShadowRoot} from '@rc-component/util/es/Dom/shadow';
+
--- oxalate-fillstation-frontend/vite.config.ts
+++ oxalate-fillstation-frontend/vite.config.ts
@@ -1,11 +1,21 @@
 import {defineConfig} from 'vite'
-import react, {reactCompilerPreset} from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import {fileURLToPath} from 'node:url'
+import {resolve} from 'node:path'
+
+const projectRoot = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: [
+            {
+                find: /^@rc-component\/util$/,
+                replacement: resolve(projectRoot, 'src/shims/rcComponentUtil.ts'),
+            },
+        ],
+    },
     plugins: [
         react(),
-        babel({presets: [reactCompilerPreset()]})
     ],
 })
