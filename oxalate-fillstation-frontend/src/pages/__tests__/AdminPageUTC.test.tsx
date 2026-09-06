064006a054f460fe1f0934ae6f9517da879ded88
Paul-Erik Törrönen <poltsi@poltsi.fi>
2026-09-06 12:42:26 +0300

    Update dependencies and enhance test configurations
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- oxalate-fillstation-frontend/src/pages/__tests__/AdminPageUTC.test.tsx
+++ oxalate-fillstation-frontend/src/pages/__tests__/AdminPageUTC.test.tsx
@@ -4,8 +4,10 @@ import type {AxiosResponse} from 'axios';
 import AdminPage from '../AdminPage';
 import {activateAdminUser, closeAdminUser, getAdminUsers, getConfig, sendAdminPasswordReset,} from '../../api/adminApi';
 
+const translate = (key: string): string => key;
+
 jest.mock('react-i18next', () => ({
-    useTranslation: () => ({t: (key: string) => key}),
+    useTranslation: () => ({t: translate}),
 }));
 
 jest.mock('../../api/adminApi', () => ({
@@ -68,6 +70,5 @@ describe('AdminPageUTC', () => {
         await user.click(screen.getByRole('button', {name: 'admin.closeUser'}));
         await user.click(await screen.findByRole('button', {name: 'common.yes'}));
         expect(mockCloseAdminUser).toHaveBeenCalledWith(11);
-    });
+    }, 10000);
 });
-
