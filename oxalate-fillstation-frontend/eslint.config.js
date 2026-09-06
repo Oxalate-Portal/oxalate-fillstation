a8650a8d49aeeac3fbaa72b4696f1510edc5c096
Paul-Erik Törrönen <poltsi@poltsi.fi>
2026-09-06 13:23:07 +0300

    Update .gitignore and ESLint configuration; remove unused files
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- oxalate-fillstation-frontend/eslint.config.js
+++ oxalate-fillstation-frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import {defineConfig, globalIgnores} from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist', '.yarn/**']),
+    globalIgnores(['dist', 'coverage', '.yarn/**']),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
